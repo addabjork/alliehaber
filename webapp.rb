830381885fe4bfdad6593be17ba0d9e7bd0f3649
addabjork <adda.birnir@gmail.com>
2013-07-08 12:04:11 -0400

    trying to get the tweets to loop

--- webapp.rb
+++ webapp.rb
@@ -7,41 +7,59 @@ require "pony"
 
 # twitter = Net::HTTP.start('twitter.com')
 
+
 Twitter.configure do |config|
   config.consumer_key = "iYXDkmpfCyT6QZp7LACotA"
   config.consumer_secret = "bFErURhhhi9jCYdar9WlGrLic8lFMakrsRZjesJAdqE"
   config.oauth_token = "78766228-FulLq2OVCn446ATNXPcpwLWlFQLz0yFV5rRqXLE0x"
   config.oauth_token_secret = "IIQz4iuTy6d0vbgRdWrkZpkeQi8kdNg0pmdhOI0ui9Y"
 end
-puts "Input your email address"
-email_address = gets.chomp
+
+#puts "Input your email address"
+#email_address = gets.chomp
 
 puts "Which account would u you like to get emails from, no spaces."
 account = gets.downcase
 timeline = Twitter.user_timeline(account)
 
+puts timeline.length
 
-timeline.each do |tweet|
-	tweetid = tweet.id
-	tweetstatus = tweet.text
-		Pony.mail({
-  				:to => email_address,
-  				:via => :smtp,
-  				:subject => "#{account} tweeted",
-  				:body => tweetstatus,
-  				:via_options => {
-    			:address              => 'smtp.gmail.com',
-    			:port                 => '587',
-    			:enable_starttls_auto => true,
-    			:user_name            => 'allieshaber',
-    			:password             => 'ahaber1315',
-    			:authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
-    			:domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
-  				}} )
-		break
+firsttweetid = timeline[0].id
+
+puts firsttweetid
+
+timeline = Twitter.user_timeline(account)
+
+secondtweetid = timeline[0].id
+
+if firsttweetid != secondtweetid
+  puts secondtweetid
+else
+  puts "hasn't tweeted since we last checked"
 end
+
+#timeline.each do |tweet|
+#	   tweetid = tweet.id
+#	   tweetstatus = tweet.text
+#		 Pony.mail({
+ # 				:to => email_address,
+  #				:via => :smtp,
+  #				:subject => "#{account} tweeted",
+  #				:body => tweetstatus,
+  ## 			:address              => 'smtp.gmail.com',
+    #			:port                 => '587',
+    #			:enable_starttls_auto => true,
+    #			:user_name            => 'allieshaber',
+    #			:password             => 'ahaber1315',
+    #			:authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+    #			:domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+  	#			}} )
+		#sleep(10)
+#end
 
 # email the tweet , use if ___ tweets something email these people,
 # elsif ___ tweets something, email these people
 
+
+#sleep 10
 
