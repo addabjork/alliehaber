28bd33e2d94946bd159e35cc5e1767f870c947ef
addabjork <adda.birnir@gmail.com>
2013-07-09 12:50:06 -0400

    football app started

---
+++ fantasyfootball/fantasyfootball.rb
@@ -0,0 +1,26 @@
+
+Allie's players:
+
+players = [ "Aaron Rodgers", 
+
+"Cam Newton, 
+Christian Ponder
+Adrian Peterson
+Marshawn Lynch
+Doug Martin
+Alfred Morris
+Brandon Marshal
+Reggie Wayne
+Ws Welker
+Victor Cruz
+
+{
+	:name => , :position =>, :nfl_team =>, :stats => {
+		:touchdowns =>
+	}
+}
+
+class FootballPlayer
+	attr_accessor :position, :stats, :name, :nfl_team
+
+end
